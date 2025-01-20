3bd9a069e1346edf926b7a9198f3a35ff59b60ba
Gary Smith <smith.gary1970@gmail.com>
2025-01-20 17:23:30 -0500

    Initial Commit

---
+++ Tiltwire.Services.Shared/Logging/SerilogExtension.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Specialized;
+using Elastic.Apm.SerilogEnricher;
+using Elastic.Serilog.Sinks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace Tiltwire.Services.Shared.Logging;
+
+public static class SerilogExtension
+{
+    //private const string serviceName = "ServiceRegistryService";
+
+    public static void AddLogging(this WebApplicationBuilder builder)
+    {
+        var cfg = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("service.name", ServiceConfig.ServiceName)
+            .Enrich.WithElasticApmCorrelationInfo()
+            .WriteTo.Console();
+
+        if (ServiceConfig.ElasticNodes != string.Empty)
+            cfg.WriteTo.Elasticsearch([new Uri(ServiceConfig.ElasticNodes)],
+                opts =>
+                {
+                    opts.MinimumLevel = LogEventLevel.Debug;
+                    opts.ConfigureChannel = channel =>
+                    {
+                        //channel.ExportResponseCallback = (response, buffer) => 
+                        //    Console.WriteLine($"Written  {buffer.Count} logs to Elasticsearch: {response.ApiCallDetails.HttpStatusCode} {Utils.GetStringFromBulkResponseBites(response)}");
+                    };
+                },
+                trans =>
+                {
+                    var headers = new NameValueCollection { { "Authorization", $"ApiKey {ServiceConfig.ElasticApiKey}" } };
+                    trans.GlobalHeaders(headers);
+                });
+
+        Log.Logger = cfg.CreateLogger();
+
+        builder.Logging.ClearProviders();
+        builder.Host.UseSerilog();
+    }
+}
---
+++ Tiltwire.Services.Shared/ServiceConfig.cs
@@ -0,0 +1,52 @@
+﻿namespace Tiltwire.Services.Shared;
+
+public static class ServiceConfig
+{
+    public static void Init(string serviceName)
+    {
+        // General
+        ServiceName = serviceName;
+        
+        // Postgres
+        PostgresHost = Environment.GetEnvironmentVariable(APP_POSTGRES_HOST) ?? "";
+        PostgresUsername = Environment.GetEnvironmentVariable(APP_POSTGRES_USERNAME) ?? "";
+        PostgresPassword = Environment.GetEnvironmentVariable(APP_POSTGRES_PASSWORD) ?? "";
+        // Elastic
+        ElasticApiKey = Environment.GetEnvironmentVariable(APP_ELASTIC_APIKEY) ?? "";
+        ElasticNodes = Environment.GetEnvironmentVariable(APP_ELASTIC_NODES) ?? "";
+
+        Console.WriteLine($"PostgresHost={PostgresHost}");
+        Console.WriteLine($"PostgresUsername={PostgresUsername}");
+        Console.WriteLine($"ElasticApiKey={ElasticApiKey}");
+        Console.WriteLine($"ElasticNodes={ElasticNodes}");
+    }
+
+    #region Constants
+
+    // Postgres
+    private const string APP_POSTGRES_HOST = "APP_POSTGRES_HOST";
+    private const string APP_POSTGRES_USERNAME = "APP_POSTGRES_USERNAME";
+    private const string APP_POSTGRES_PASSWORD = "APP_POSTGRES_PASSWORD";
+
+    // Elastic
+    private const string APP_ELASTIC_APIKEY = "APP_ELASTIC_APIKEY";
+    private const string APP_ELASTIC_NODES = "APP_ELASTIC_NODES";
+
+    #endregion
+
+    #region Properties
+
+    // General
+    public static string ServiceName { get; set; } = string.Empty;
+    
+    // Postgres
+    public static string PostgresHost { get; set; } = string.Empty;
+    public static string PostgresUsername { get; set; } = string.Empty;
+    public static string PostgresPassword { get; set; } = string.Empty;
+
+    // Elastic
+    public static string? ElasticApiKey { get; set; } = string.Empty;
+    public static string ElasticNodes { get; set; } = string.Empty;
+
+    #endregion
+}
